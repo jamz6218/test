d98e0ada9fada322853135a15e0b92d359bd7577
jamaal akhtar <j.akhtar1@newcastle.ac.uk>
2023-11-12 22:32:14 +0000

    test commit

--- src/Main.java
+++ src/Main.java
@@ -10,6 +10,8 @@ public class Main {
     public static void main(String[] args) {
         Date now = new Date();
         System.out.println(now);
+        System.out.println("TEST");
+
 
     }
 }
