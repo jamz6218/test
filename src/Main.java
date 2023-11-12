e14aa93f78474c5c708e411addd984aeb04426ac
jamaal akhtar <j.akhtar1@newcastle.ac.uk>
2023-11-12 21:50:25 +0000

    First commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+/**
+ * java doc comment to describe how to use code
+ * use one * for normal comments
+ */
+
+// package com.package;
+import java.util.Date;
+
+public class Main {
+    public static void main(String[] args) {
+        Date now = new Date();
+        System.out.println(now);
+
+    }
+}
